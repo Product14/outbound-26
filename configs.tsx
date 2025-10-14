c6d8c165cf4f38b6d1bc79eb68477225ce8d74c4
Product14 <product@spyne.ai>
2025-10-14 13:04:27 +0530

    route_base_url fixed

--- configs.tsx
+++ configs.tsx
@@ -1,6 +1,6 @@
 export const configs = {
     base_url: "https://api.spyne.ai/",
-    route_base_url: "https://consolespyne.ai/",
+    route_base_url: "https://console.spyne.ai/",
     // Polling configuration
     enablePolling: false, // Set to true to enable automatic data refresh
     pollingIntervalSeconds: 10 // Interval for automatic refresh when enabled
